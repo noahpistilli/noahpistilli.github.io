5e2b3a997ca71ea7ac8f0bdaf73e836884a69ab5
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-02-04 14:14:03 -0500

    Update mkwiipatcher.sh

--- RC24_Patcher/Wiimmfi-stuff/mkwiipatcher.sh
+++ RC24_Patcher/Wiimmfi-stuff/mkwiipatcher.sh
@@ -48,7 +48,7 @@ ui() {
 	then
     		printf "I couldn't find Mario Kart Wii. Put it in the same directory as the script.\n"
 		
-		read -n 1 -r -p "the RiiConnect24 Patcher" 
+		read -n 1 -r -p "Press any key to exit. " 
 		
 		exit
 	else
