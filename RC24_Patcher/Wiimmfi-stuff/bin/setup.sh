e0b62a1a4693725ff4d689a871fc74149e596252
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-02-27 09:24:40 -0500

    Update setup.sh

--- RC24_Patcher/Wiimmfi-stuff/bin/setup.sh
+++ RC24_Patcher/Wiimmfi-stuff/bin/setup.sh
@@ -233,6 +233,11 @@ function patch_mkw
 		|| error_exit $ERR_ERROR "Error while creating image: $DESTIMG"
 
     true
+    
+    clear 
+    printf "\n\nPatching has completed! You will find the patched game in the folder \"wiimmfi-images\".\n\n"
+    
+    read -n 1 -r "Press any key to return to the patcher"
 }
 
 #
