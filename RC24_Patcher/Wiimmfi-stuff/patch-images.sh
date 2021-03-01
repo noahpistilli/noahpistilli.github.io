29ff263d79a1e22af17e697970bcf43dd644a88d
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-03-01 11:02:19 -0500

    Update patch-images.sh

--- RC24_Patcher/Wiimmfi-stuff/patch-images.sh
+++ RC24_Patcher/Wiimmfi-stuff/patch-images.sh
@@ -8,6 +8,15 @@ PRINT_TITLE=2
 sketchget() {
  	curl --create-dirs -f -k -L -o ${2} -S -s https://sketchmaster2001.github.io/RC24_Patcher/${1}
 } 
+
+exitscreen() {
+	clear
+ 	print_title
+ 	printf "Patching has completed! You will find the patched game in the folder \"wiimmfi-images\".\n\n" | fold -s -w "$(tput cols)"
+ 	read -n 1 -r -p "Press any key to return to the patcher: "
+	
+ 	exit
+}
  
  sketchget "Wiimmfi-stuff/wit${sys}" wit 
  chmod +x wit
@@ -76,17 +85,10 @@ do
 	    printf '\n%b Patch MKW image: %s %b\n' "$COL_HEAD" "$src" "$COL0"
 	    patch_mkw "$src" "$dest" "$res_type_option" 2>&1
 	fi
-	printf '\n'
-
-    clear
-    printf "Patching has completed! You will find the patched game in the folder \"wiimmfi-images\".\n\n" | fold -s -w "$(tput cols)"
-    read -n 1 -r -p "Press any key to return to the patcher: "
-	
-    exit
-    
+   
     ) 2>&1 | tee "$log"
 	
 done
 
-
+exitscreen
 
